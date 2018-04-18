0518d132545fe936a5653531887d74fb8a758f20
zhaozhihui <121721272@qq.com>
2018-04-18 19:47:31 +0800

    创建建项目

---
+++ gitDemo/gitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by 赵志辉 on 2018/4/18.
+//  Copyright © 2018年 赵志辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ gitDemo/gitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by 赵志辉 on 2018/4/18.
+//  Copyright © 2018年 赵志辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
